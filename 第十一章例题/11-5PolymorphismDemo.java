0e904fb62a63d85fba909422bd9385ecc09f4be0
panminjia <54836755+panminjia@users.noreply.github.com>
2019-12-10 15:24:18 +0800

    Add files via upload

---
+++ 第十一章例题/11-5PolymorphismDemo.java
@@ -0,0 +1,13 @@
+
+public class PolymorphismDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayObject(new CircleFromSimpleGeometricObject(1, "red", false));
+		displayObject(new RectangleFromSimpleGeometricObject(1,1, "black", true));
+	}
+	public static void displayObject(SimpleGeometricObject object) {
+		System.out.println("Created on " + object.getDateCreated() + ".Color is " + object.getColor());
+	}
+
+}
